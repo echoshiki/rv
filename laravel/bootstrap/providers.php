5d1638b8f66e1f3df6fb2eef443dd334b20cee41
echoshiki <>
2025-04-26 22:04:56 +0800

    安装 telescope

--- laravel/bootstrap/providers.php
+++ laravel/bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\EasywechatServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
