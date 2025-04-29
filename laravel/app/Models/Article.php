562abf575cdc4926a4b9ad2c2ec638965ca87189
echoshiki <brokenworldand.me@gmail.com>
2025-04-29 17:47:20 +0800

    create article

---
+++ laravel/app/Models/Article.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Container\Attributes\Storage;
+
+class Article extends Model
+{
+    /** @use HasFactory<\Database\Factories\ArticleFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 
+        'category_id', 
+        'title', 
+        'cover', 
+        'content',
+        'sort', 
+        'is_active',
+        'published_at'
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'is_active' => 'boolean',
+            'sort' => 'integer',
+            'published_at' => 'datetime'
+        ];
+    }
+
+    /**
+     * 模型事件 - 创建监听注册器
+     * creating 事件 (创建记录时触发)
+     * updating 事件 (更新记录时触发)
+     * deleting 事件 (删除记录时触发)
+     */
+    protected static function booted(): void
+    {
+        static::creating(function (Article $article) {
+            // 如果用户名为空，则填入登录用户id
+            if (empty($article->user_id) && Auth::check()) {
+                $article->user_id = Auth::id();
+            }
+        });
+
+        static::updating(function (Article $article) {
+            if ($article->isDirty('cover')) {
+                $oldCover = $article->getOriginal('cover');
+                if ($oldCover) {
+                    Storage::disk('public')->delete($oldCover);
+                }
+            }
+        });
+    }
+
+
+}
---
+++ laravel/database/migrations/2025_04_29_074034_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->comment('作者ID');
+            $table->foreignId('category_id')->comment('文章分类ID');
+            $table->string('title')->comment('文章标题');
+            $table->string('cover')->nullable()->comment('封面图');
+            $table->longText('content')->comment('文章内容');
+            $table->integer('sort')->default(0)->comment('排序');
+            $table->boolean('is_active')->default(true)->index()->comment('是否启用');
+            $table->timestamp('published_at')->nullable()->comment('发布时间');
+            $table->timestamps();
+            
+            $table->index('published_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
