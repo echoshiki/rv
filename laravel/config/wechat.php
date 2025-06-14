cc7485e29554ce725a5547de517e36d97ba47cce
echoshiki <>
2025-06-14 17:29:10 +0800

    完善微信支付配置文件

--- laravel/config/wechat.php
+++ laravel/config/wechat.php
@@ -17,17 +17,18 @@ return [
     
     // 微信支付相关配置
     'pay' => [
-        'app_id'     => env('WECHAT_MINI_APP_ID'),
-        'secret'     => env('WECHAT_MINI_APP_SECRET'),
-        'mch_id'     => env('WECHAT_PAY_MCH_ID'),
-        'secret_key' => env('WECHAT_PAY_API_V3_KEY'),
-        'private_key' => env('WECHAT_PAY_PRIVATE_KEY'),
-        'certificate' => env('WECHAT_PAY_CERTIFICATE'),
-        'platform_certs' => env('WECHAT_PAY_PLATFORM_CERTS'),
-        'v2_secret_key' => env('WECHAT_PAY_API_V2_KEY'),
+        'app_id'     => env('WECHAT_MINI_APP_ID', ''),
+        'secret'     => env('WECHAT_MINI_APP_SECRET', ''),
+        'mch_id'     => env('WECHAT_PAY_MCH_ID', ''),
+        'secret_key' => env('WECHAT_PAY_API_V3_KEY', ''),
+        'private_key' => env('WECHAT_PAY_PRIVATE_KEY', ''),
+        'certificate' => env('WECHAT_PAY_CERTIFICATE', ''),
+        'serial_no'   => env('WECHAT_PAY_SERIAL_NO', ''),
+        'platform_certs' => env('WECHAT_PAY_PLATFORM_CERTS', ''),
+        'platform_public_key' => env('WECHAT_PAY_PUBLIC_KEY', ''),
         'notify_url' => env('WECHAT_PAY_NOTIFY_URL', '/api/v1/payments/notify/wechat'),
         'http' => [
-            'throw'  => true, // 状态码非 200、300 时是否抛出异常，默认为开启
+            'throw'  => true,
             'timeout' => 5.0
         ]
     ],
