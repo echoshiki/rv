20d49565855fac6ab88085a78a672032c3c89fa3
echoshiki <brokenworldand.me@gmail.com>
2025-06-16 18:09:12 +0800

    证书下载地址

--- laravel/config/wechat.php
+++ laravel/config/wechat.php
@@ -14,8 +14,26 @@ return [
             'retry' => true, // 使用默认重试配置
         ],
     ],
+
+    // [
+    //     "app_id" => "wxe3d1ce1694982eb5",
+    //     "secret" => "a6f530cc3a742e8c2ce1b37e3f50b762",
+    //     "mch_id" => "1719428406",
+    //     "secret_key" => "DczDr8GENESqMxSaB2Ub5dhMk4DkYILz",
+    //     "private_key" => "/www/rv/laravel/storage/certs/wechat_pay/apiclient_key.pem",
+    //     "certificate" => "/www/rv/laravel/storage/certs/wechat_pay/apiclient_cert.pem",
+    //     "serial_no" => "",
+    //     "platform_certs" => "",
+    //     "platform_public_key" => "",
+    //     "notify_url" => "",
+    //     "http" => [
+    //       "throw" => true,
+    //       "timeout" => 5.0,
+    //     ],
+    // ]
     
     // 微信支付相关配置
+    // https://github.com/wechatpay-apiv3/wechatpay-php#%E5%A6%82%E4%BD%95%E4%B8%8B%E8%BD%BD%E5%B9%B3%E5%8F%B0%E8%AF%81%E4%B9%A6
     'pay' => [
         'app_id'     => env('WECHAT_MINI_APP_ID', ''),
         'secret'     => env('WECHAT_MINI_APP_SECRET', ''),
@@ -25,6 +43,7 @@ return [
         'certificate' => storage_path('certs/wechat_pay/apiclient_cert.pem'),
         'serial_no'   => env('WECHAT_PAY_SERIAL_NO', ''),
         'platform_certs' => [
+            '/path/to/wechatpay/cert.pem',
             "PUB_KEY_ID_0117194284062025061600332347001803" => storage_path('certs/wechat_pay/pub_key.pem'),
         ],
         'notify_url' => env('WECHAT_PAY_NOTIFY_URL', '/api/v1/payments/notify/wechat'),
